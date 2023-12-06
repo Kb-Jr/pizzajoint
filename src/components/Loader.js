3fc2e0625ce8094b116c2c6612b8d5c62855c6ec
Kb-Jr <abdulkabiruj@gmail.com>
2023-12-06 16:07:06 +0100

    Loader

---
+++ src/components/Loader.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Loader = () => {
+  const Loaderanimation = {
+    animation: {
+      x: [-20, 20],
+      y: [0, -30],
+      transition: {
+        x: {
+          yoyo: Infinity,
+          duration: 0.5,
+        },
+
+        y: {
+          yoyo: Infinity,
+          duration: 0.25,
+          ease: "easeOut",
+        },
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      className="loader"
+      variants={Loaderanimation}
+      animate="animation"
+    ></motion.div>
+  );
+};
+
+export default Loader;
